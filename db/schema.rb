33d65c8e8cc226520dfd04057b2de34c2dd54e1b
Alexandria Brown <axbn2190@gmail.com>
2018-03-20 11:05:54 -0700

    created schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320171051) do
+ActiveRecord::Schema.define(version: 20180320174320) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
 end
